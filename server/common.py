5fc80bfec5902f97ea2dfc095166903dc1492a77
Mateus Pinto da Silva <mateusps10@outlook.com>
2021-09-16 09:29:26 -0300

    still needing microtransac system

--- server/common.py
+++ server/common.py
@@ -14,6 +14,9 @@ def router(request, database):
         'create_sticker_model': rt.create_sticker_model,
         'print_stickers': rt.print_stickers,
         'buy_sticker_pack': rt.buy_sticker_pack,
+        'paste_sticker': rt.paste_sticker,
+        'view_album': rt.view_album,
+        'view_all_unpasted_stickers': rt.view_all_unpasted_stickers,
     }
 
     return MW[request['method']](request, database)
--- server/routes.py
+++ server/routes.py
@@ -110,7 +110,7 @@ def print_stickers(request, database):
 
     DB_CUR = database.cursor()
     for _ in range(request["sticker_print_number"]):
-        DB_CUR.execute(f'INSERT INTO sticker_prints (sticker_models_name, owner_user_name, is_pasted, is_for_sale, sale_price) VALUES ("{request["sticker_model_name"]}", "", 0, 0, 0)')
+        DB_CUR.execute(f'INSERT INTO sticker_prints (sticker_models_name, owner_user_email, is_pasted, is_for_sale, sale_price) VALUES ("{request["sticker_model_name"]}", "", 0, 0, 0)')
     database.commit()
     return {'error': 0}
 
@@ -118,17 +118,59 @@ def print_stickers(request, database):
 def buy_sticker_pack(request, database):
     DB_CUR = database.cursor()
 
-    if list(DB_CUR.execute('SELECT COUNT(*) FROM sticker_prints WHERE owner_user_name=""'))[0][0] < 2:
+    if list(DB_CUR.execute('SELECT COUNT(*) FROM sticker_prints WHERE owner_user_email=""'))[0][0] < 2:
         return {'error': 1, 'error_message': 'Falha na compra do pacote de figurinhas! Não existe figurinhas impressas sem dono suficientes'}
 
     if get_coins(request, database)['coins'] < 10:
         return {'error': 1, 'error_message': 'Falha na compra do pacote de figurinhas! Você não tem moedas o suficiente!'}
 
     DB_CUR.execute(f'UPDATE users SET coins=coins-10 WHERE email="{request["email"]}"')
-    WON_STICKERS = random.sample(list(DB_CUR.execute('SELECT id, sticker_models_name FROM sticker_prints WHERE owner_user_name=""')), 2)
+    WON_STICKERS = random.sample(list(DB_CUR.execute('SELECT id, sticker_models_name FROM sticker_prints WHERE owner_user_email=""')), 2)
     for sticker in WON_STICKERS:
-        DB_CUR.execute(f'UPDATE sticker_prints SET owner_user_name="{request["email"]}" WHERE id="{sticker[0]}"')
+        DB_CUR.execute(f'UPDATE sticker_prints SET owner_user_email="{request["email"]}" WHERE id="{sticker[0]}"')
 
     database.commit()
 
     return {'error': 0, 'won_stickers': [{'id': idf, 'sticker_model_name': sticker_model_name} for idf, sticker_model_name in WON_STICKERS]}
+
+
+def __is_this_sticker_model_pasted(request, database):
+    DB_CUR = database.cursor()
+
+    if list(DB_CUR.execute(f'SELECT COUNT(*) FROM sticker_prints WHERE id="{request["sticker_print_id"]}" AND owner_user_email="{request["email"]}" AND is_pasted=1'))[0][0] == 1:
+        return {'error': 0}
+    else:
+        return {'error': 1, 'error_message': 'O modelo de figurinha não está colado!'}
+
+
+def __is_this_sticker_print_owned_by_the_user(request, database):
+    DB_CUR = database.cursor()
+
+    if list(DB_CUR.execute(f'SELECT COUNT(*) FROM sticker_prints WHERE id="{request["sticker_print_id"]}" AND owner_user_email="{request["email"]}"'))[0][0] == 1:
+        return {'error': 0}
+    else:
+        return {'error': 1, 'error_message': 'O usuário não é dono desta figurinha!'}
+
+
+def paste_sticker(request, database):
+    if __is_this_sticker_model_pasted(request, database)['error'] == 0:
+        return {'error': 1, 'error_message': 'Não é possível colar esta figurinha! O modelo de figurinha já está colado.'}
+
+    if __is_this_sticker_print_owned_by_the_user(request, database)['error'] == 1:
+        return {'error': 1, 'error_message': 'Erro ao colar figurinha! O usuário não é dono desta figurinha.'}
+
+    DB_CUR = database.cursor()
+    DB_CUR.execute(f'UPDATE sticker_prints SET is_pasted=1 WHERE id="{request["sticker_print_id"]}"')
+    database.commit()
+
+    return {'error': 0}
+
+
+def view_album(request, database):
+    DB_CUR = database.cursor()
+    return {'error': 0, 'stickers': [x[0] for x in list(DB_CUR.execute(f'SELECT sticker_models_name FROM sticker_prints WHERE owner_user_email="{request["email"]}" AND is_pasted=1'))]}
+
+
+def view_all_unpasted_stickers(request, database):
+    DB_CUR = database.cursor()
+    return {'error': 0, 'stickers': [{'id': x[0], 'sticker_model_name': x[1]} for x in list(DB_CUR.execute(f'SELECT id, sticker_models_name FROM sticker_prints WHERE owner_user_email="{request["email"]}" AND is_pasted=0'))]}
